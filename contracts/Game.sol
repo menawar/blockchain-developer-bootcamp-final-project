22551d2d6092b688f95100907d889bae3d0b336c
Sylvester Menawar <sylvesternathan93@gmail.com>
2021-11-18 17:03:13 +0000

    added tokenURI function to Game.sol

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "./libraries/Base64.sol";
 import "hardhat/console.sol";
 
 contract Game is ERC721 {
@@ -98,5 +99,48 @@ contract Game is ERC721 {
 
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId)
+    public
+    view
+    override
+    returns (string memory)
+  {
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(
+      charAttributes.attackDamage
+    );
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            " -- NFT #: ",
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',
+            strHp,
+            ', "max_value":',
+            strMaxHp,
+            '}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,
+            "} ]}"
+          )
+        )
+      )
+    );
+
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    return output;
   }
 }
